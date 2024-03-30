e6cae879c7e4a006c615538e1d82034bc3e3c7a2
Shubham <cptnshubham39@gmail.com>
2024-03-30 14:37:10 +0000

    Update face_Rec.py

--- face_Rec.py
+++ face_Rec.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
+import subprocess
 import sqlite3
 import os
+import time
+
+from time import sleep
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -10,7 +14,22 @@ c = conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS faces
              (id INTEGER PRIMARY KEY, name TEXT)''')
-
+def image_capture():
+    rm_command = "rm -rf faces/cache.jpg"
+    libcamera_command = ["libcamera-still", "-o", "faces/cache.jpg", "--nopreview", "--timeout","10"]
+    try:
+        result = subprocess.run(rm_command, shell=True)
+        if result.returncode != 0:
+            raise RuntimeError(f"Error removing cache image: {result.stderr}")
+    except Exception as e:
+        raise RuntimeError(f"Error during cleanup: {e}")
+    try:
+        result = subprocess.run(libcamera_command)
+        if result.returncode != 0:
+            raise RuntimeError(f"Error capturing image: {result.stderr}")
+    except Exception as e:
+        raise RuntimeError(f"Error during image capture: {e}")
+        
 def detect_faces(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
@@ -47,18 +66,18 @@ directory = "faces"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    image_capture()
+    image_path = "faces/cache.jpg"
+    frame = cv2.imread(image_path)
 
     recognize_face(frame)
 
     cv2.putText(frame, "Press 'r' to register a new face with a name.", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
     cv2.imshow('Face Recognition', frame)
+    time.sleep(2)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
@@ -67,7 +86,6 @@ while True:
         name = input("Enter the name to register: ")
         store_face(name, frame)
 
-cap.release()
 cv2.destroyAllWindows()
 
 conn.close()
